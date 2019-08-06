f4f524d6fea5451fa48999dae9ddde08a884d0eb
github1 <john.collier4@gmail.com>
2019-08-05 21:25:54 -0400

    remove json dir

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,7 +2,7 @@
 
 DIST="./dist"
 mkdir -p "${DIST}"
-cp -R package.json json scripts "${DIST}/"
+cp -R package.json "${DIST}/"
 if [[ -f "${AUX_PREPARE_SCRIPT}" ]]; then
   "${AUX_PREPARE_SCRIPT}" "${DIST}/"
 fi
