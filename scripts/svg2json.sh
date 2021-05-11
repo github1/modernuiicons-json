67db86da98aea028d97485d1c5df7e746e9c0690
github1 <john.collier4@gmail.com>
2021-05-11 00:09:55 -0400

    always clone icons repo

--- scripts/svg2json.sh
+++ scripts/svg2json.sh
@@ -2,11 +2,11 @@
 
 rm -rf ./dist/json
 mkdir -p ./dist/json
-if [[ ! -d './tmp/icons' ]]; then
-  git clone https://github.com/Templarian/WindowsIcons.git ./tmp/icons
-fi
+rm -rf ./tmp/icons
+git clone https://github.com/Templarian/WindowsIcons.git ./tmp/icons
 cd ./tmp/icons
 git pull
+rm -rf ./tmp/icons/.git
 cd -
 
 node ./scripts/svg2json.js ./dist/json/ $(find ./tmp/icons/WindowsPhone/svg -name '*.svg' -print0 | tr '\000' ',')
